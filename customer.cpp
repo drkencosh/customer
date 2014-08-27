d1469d85f9e88232ddb4467fc44d202414a5b442
drkencosh <drkencosh@gmail.com>
2014-08-27 19:47:45 +0700

    partial customer class

---
+++ customer.cpp
@@ -0,0 +1,31 @@
+class customer
+{
+public:
+	customer() { }
+	customer(int min, int max) {
+		int diff = max-min;
+		arrival = rand() % 240;
+		service_time = (rand() % diff) + min;
+	}
+	customer(const customer &cus)
+	{
+		arrival = cus.get_arr();
+		service_time = cus.get_ser();
+	}
+	int get_arr() const{ return arrival; }
+	int get_ser() const{ return service_time; }
+	friend bool operator <(customer a, customer b)
+	{
+		if(a.arrival<b.arrival)
+		{ return true;}	else{ return false;}
+	}
+	customer operator =(customer& rhs)
+	{
+		arrival = rhs.get_arr();
+		service_time = rhs.get_ser();
+		return *this;
+	}
+private:
+	int arrival;
+	int service_time;
+};
